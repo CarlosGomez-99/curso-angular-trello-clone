2a748b54cfd7d6060146d8c7ffcbb99b212ae5a5
carlos.gomez <drcgomez@hotmail.com>
2024-05-06 17:10:04 -0500

    typing colors

--- src/app/modules/shared/components/card-color/card-color.component.ts
+++ src/app/modules/shared/components/card-color/card-color.component.ts
@@ -1,45 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { COLORS, Colors } from '@models/colors.models';
 
 @Component({
   selector: 'app-card-color',
   templateUrl: './card-color.component.html',
 })
 export class CardColorComponent {
-  @Input() color: 'sky' | 'yellow' | 'green' | 'red' | 'violet' | 'gray' =
-    'sky';
+  @Input() color: Colors = 'sky';
 
-  mapColorToClass = {
-    sky: {
-      'bg-sky-700': true,
-      'hover:bg-sky-700': true,
-      'text-white': true,
-    },
-    yellow: {
-      'bg-yellow-700': true,
-      'hover:bg-yellow-700': true,
-      'text-white': true,
-    },
-    green: {
-      'bg-green-700': true,
-      'hover:bg-green-700': true,
-      'text-white': true,
-    },
-    red: {
-      'bg-red-700': true,
-      'hover:bg-red-700': true,
-      'text-white': true,
-    },
-    violet: {
-      'bg-violet-700': true,
-      'hover:bg-violet-700': true,
-      'text-white': true,
-    },
-    gray: {
-      'bg-gray-700': true,
-      'hover:bg-gray-700': true,
-      'text-white': true,
-    },
-  };
+  mapColorToClass = COLORS;
 
   get colorClass() {
     const classes = this.mapColorToClass[this.color];
